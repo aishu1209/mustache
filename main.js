d7c9535948957e489cf8aba7c5baf19d3b4fcc68
aishu1209 <89282363+aishu1209@users.noreply.github.com>
2022-07-28 15:08:19 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload(){
+
+
+}
+
+function setup(){
+canvas = createCanvas(640,480);
+canvas.center();
+}
+
+function draw(){
+
+}
+
+function takeSnapshot(){
+    save('mustache_pic.png');
+    
+}
